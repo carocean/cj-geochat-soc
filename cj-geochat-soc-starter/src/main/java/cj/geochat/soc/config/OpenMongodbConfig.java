bf563622036ec9dd0e7f7242e98e716150299668
cj <carocean.jofers@icloud.com>
2023-08-19 14:48:04 +0800

    1.0.0

---
+++ cj-geochat-soc-starter/src/main/java/cj/geochat/soc/config/OpenMongodbConfig.java
@@ -0,0 +1,8 @@
+package cj.geochat.soc.config;
+
+import cj.geochat.ability.mongodb.annotation.EnableCjMongoDB;
+import org.springframework.context.annotation.Configuration;
+@EnableCjMongoDB
+@Configuration
+public class OpenMongodbConfig {
+}
